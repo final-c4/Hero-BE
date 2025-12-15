e877e60b8b51cea5288918476c28ab6b682d822c
haenin <heyone0022@gmail.com>
2025-12-16 01:51:55 +0900

    [Feat] 알림 엔티티 필요하지 않은 메서드 삭제와 테이블명 수정

--- hero-be/src/main/java/com/c4/hero/domain/notification/entity/Notification.java
+++ hero-be/src/main/java/com/c4/hero/domain/notification/entity/Notification.java
@@ -21,7 +21,7 @@ import java.time.LocalDateTime;
  */
 
 @Entity
-@Table(name = "tbl_notifications")
+@Table(name = "tbl_notification")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Notification {
@@ -78,10 +78,5 @@ public class Notification {
             this.isRead = false;
         }
     }
-
-    // 읽음 처리 메서드
-    public void markAsRead() {
-        this.isRead = true;
-        this.readAt = LocalDateTime.now();
-    }
+    
 }
