2eb31be769d9d027700c4cdf9e7ba7b4f5cd489d
haenin <gpdnjsqhdld@naver.com>
2026-01-02 19:21:33 +0900

    [Fix] 임시 배포

--- hero-be/src/main/java/com/c4/hero/common/config/SecurityConfig.java
+++ hero-be/src/main/java/com/c4/hero/common/config/SecurityConfig.java
@@ -158,7 +158,6 @@ public class SecurityConfig {
         // 허용할 Origin (프론트엔드 주소)
         configuration.setAllowedOrigins(Arrays.asList(
                 "https://hero-hr.site",
-                "https://www.hero-hr.site",
                 "http://localhost:8080" // 개발환경
         ));
 
